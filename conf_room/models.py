1bce17deaed5cc8dd88884ba200f3457ba44a3f3
Sunip88 <drzep4@gmail.com>
2018-10-20 12:42:54 +0200

    Templates and models

--- conf_room/models.py
+++ conf_room/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=64)
+    capacity = models.IntegerField()
+    projector = models.BooleanField(default=False)
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    rooms = models.ForeignKey(Room, on_delete=models.CASCADE)
+    comment = models.TextField()
